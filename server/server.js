fa652ea5729a9afdf2a6a3e68d973ea82acecdce
Narendran <82641246+NarendranKT@users.noreply.github.com>
2023-08-05 12:08:52 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT =  8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-    origin : [""],
+    origin : ["https://deploy-mern-1whq-vercel.app"],
     methods: ["POST", "GET", "PUSH", "DELETE"],
 ));
 
