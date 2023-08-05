d4ccf4915ffb3d3163c2629dbb7cc725e4501dfc
Narendran <82641246+NarendranKT@users.noreply.github.com>
2023-08-05 11:56:53 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,10 @@ const PORT =  8000;
 // $MiddleWares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({
+    origin : [""],
+    methods: ["POST", "GET", "PUSH", "DELETE"],
+));
 
 // $Routes
 app.use("/company", router);
